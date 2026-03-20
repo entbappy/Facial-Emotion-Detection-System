5cf701279f7980d5aa545080e3ce33514db2b638
entbappy <entbappy73@gmail.com>
2026-03-20 06:07:42 +0600

    structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="facial_emotion_detection_system",
+    version="0.1.0",
+    author="Bappy",
+    author_email="entbappy73@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
